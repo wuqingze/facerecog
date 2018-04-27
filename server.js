a0d0af0342035026d5cb48dc3146d89c6ae38e3c
wqngze0 <10152510231@stu.ecnu.edu.cn>
2018-04-27 20:23:26 +0800

    test

--- server.js
+++ server.js
@@ -26,7 +26,9 @@ io.on('connection', function(socket){
     });
 });
 
-http.listen(8080, function(){
+var port = process.env.PORT || 1337;
+
+http.listen(port, function(){
     console.log('listening on *:8080');
   });
  
